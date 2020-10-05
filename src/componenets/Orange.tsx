1f60c60e695e2bd50ea0f53da502fa74ee1bdb0d
Karolina Grochal <46192501+caomei1806@users.noreply.github.com>
2020-10-05 12:21:59 +0200

    :grey_question: :purple_heart: Orange Animation: Linear Slider added

--- src/componenets/Orange.tsx
+++ src/componenets/Orange.tsx
@@ -3,11 +3,9 @@ import React from "react";
 export const Orange = () => {
   return (
     <div className="orange-screen">
-      <div className="slide-card">
+      <div className="slide-card-linear mb-3">
+        <i className="bx bx-question-mark"></i>
         <div className="inner">
-          <span>
-            <i className="bx bx-right-arrow-alt"></i>
-          </span>
           <h1>Hello</h1>
           <p>
             Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe
@@ -15,6 +13,17 @@ export const Orange = () => {
             distinctio fuga voluptatum?
           </p>
         </div>
+      </div>
+      <div className="slide-card-circle">
+        <span>
+          <i className="bx bx-right-arrow-alt"></i>
+        </span>
+        <h1>Hello</h1>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe
+          facilis suscipit corrupti quidem aliquid veniam repudiandae libero
+          distinctio fuga voluptatum?
+        </p>
       </div>
     </div>
   );
